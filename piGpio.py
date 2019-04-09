b6c23db342b7e4343299b799e1ca5f2e1670d974
Aaron <aarontwillcock@gmail.com>
2019-04-09 16:10:22 -0400

    piGpio.py - Finished merge of conditional gpio on/off/toggle

--- piGpio.py
+++ piGpio.py
@@ -12,17 +12,13 @@ class gpioPin:
         self.enabled = enabled
 
         #Initialize pin as input or output
-<<<<<<< HEAD
         self.out = out
-=======
->>>>>>> e88ba93eb52cb017908453fed87c269889fefae5
         if(out):
             gpio.setup(self.pin, gpio.OUT)
         else:
             gpio.setup(self.pin, gpio.IN, pull_up_down=gpio.PUD_DOWN)
 
     def on(self):
-<<<<<<< HEAD
         if(self.out):
             gpio.output(self.pin,gpio.HIGH)
             self.enabled = 1
@@ -44,17 +40,3 @@ class gpioPin:
             gpio.add_event_detect(self.pin,gpio.RISING,fxn,debounceMs)
         else:
             gpio.add_event_detect(self.pin,gpio.FALLING,fxn,debounceMs)
-=======
-        gpio.output(self.pin,gpio.HIGH)
-        self.enabled = 1
-
-    def off(self):
-        gpio.output(self.pin,gpio.LOW)
-        self.enabled = 0
-
-    def toggle(self):
-        if(self.enabled):
-            self.off()
-        else:
-            self.on()
->>>>>>> e88ba93eb52cb017908453fed87c269889fefae5
