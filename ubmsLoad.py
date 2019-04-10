e29ed21fa6a536f9f413b86d52e637a582cabdb0
Aaron <aarontwillcock@gmail.com>
2019-04-10 13:40:08 -0400

    Reconfigured load constructor

--- ubmsLoad.py
+++ ubmsLoad.py
@@ -23,11 +23,11 @@ class uLoadReq:
 
         #Two arguments (token, uLoadArgs)
         if(L==2):
-            self.token = args[0]
-            self.__dict__.update(vars(args[1]))
+            self.token = args[L]
+            self.__dict__.update(vars(args[:L]))
         elif(L==12):
-            self.token = args[0]
-            load = uLoad(args[1:])
+            self.token = args[L]
+            load = uLoad(args[:L])
             self.__dict__.update(vars(load))
 
 #Reply template for BMS to accept / reject load request
@@ -38,6 +38,6 @@ class uLoadReqReply:
         L = len(args)
 
         if(L == 2):
-            self.token  = args[0]
-            self.supplyError = args[1]
+            self.token  = args[1]
+            self.supplyError = args[0]
 
