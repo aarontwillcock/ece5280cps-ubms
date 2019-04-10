f183d48edeb9d49bca5222ac6e72c33e80ab2095
Aaron <aarontwillcock@gmail.com>
2019-04-10 08:50:47 -0400

    simplePolling.py - Removed SHDN hold and flipped CLR command logic (I misread the board)

--- simplePolling.py
+++ simplePolling.py
@@ -8,18 +8,17 @@ gpio.setmode(gpio.BCM)
 
 #Setup pin
 gpio.setup(13,gpio.IN, pull_up_down=gpio.PUD_DOWN)
-gpio.setup(26,gpio.OUT)
 gpio.setup(19,gpio.OUT)
 
-#Turn off shutdown
-gpio.output(26,gpio.LOW)
+#Disable the clear
+gpio.output(19,gpio.LOW)
 
 while True:
     if(not gpio.input(13)):
         print("Interrupted!")
     
-        gpio.output(19,gpio.HIGH)
         gpio.output(19,gpio.LOW)
+        gpio.output(19,gpio.HIGH)
     
     else:
         print("Pin high (no interrupt)")
