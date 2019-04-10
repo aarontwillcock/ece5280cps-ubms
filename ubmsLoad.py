d8c82d94642324ae3cda59669230c7d121cc0c47
Aaron <aarontwillcock@gmail.com>
2019-04-10 14:07:11 -0400

    *.py - Fixed faulty uLoad constructor, reverted "off-by-one" errors (which did not exist)

--- ubmsLoad.py
+++ ubmsLoad.py
@@ -1,17 +1,36 @@
 #Load request class for modeling an energy request from the BMS
 class uLoad:
     def __init__(self,args):
-        self.Vmin =         args[0]
-        self.Vmax =         args[1]
-        self.Imin =         args[2]
-        self.Imax =         args[3]
-        self.Pmin = self.Vmin*self.Imin
-        self.Pmax = self.Vmax*self.Imax
-        self.releaseTime =  args[4] 
-        self.duration =     args[5]
-        self.Emin = self.Pmin*self.duration
-        self.Emax = self.Pmax*self.duration
-        self.deadline =     args[6]
+
+        #Determine number of arguments
+        L = len(args)
+
+        #7 Arguments
+        if(L == 7):
+            self.Vmin =         args[0]
+            self.Vmax =         args[1]
+            self.Imin =         args[2]
+            self.Imax =         args[3]
+            self.Pmin = self.Vmin*self.Imin
+            self.Pmax = self.Vmax*self.Imax
+            self.releaseTime =  args[4] 
+            self.duration =     args[5]
+            self.Emin = self.Pmin*self.duration
+            self.Emax = self.Pmax*self.duration
+            self.deadline =     args[6]
+        else:
+            self.Vmin =         args[0]
+            self.Vmax =         args[1]
+            self.Imin =         args[2]
+            self.Imax =         args[3]
+            self.Pmin =         args[4]
+            self.Pmax =         args[5]
+            self.releaseTime =  args[6] 
+            self.duration =     args[7]
+            self.Emin =         args[8]
+            self.Emax =         args[9]
+            self.deadline =     args[10]
+
 
 #Load request template for LMS to request supply
 class uLoadReq:
@@ -26,8 +45,8 @@ class uLoadReq:
             self.token = args[0]
             self.__dict__.update(vars(args[1]))
         elif(L==12):
-            self.token = args[L-1]
-            load = uLoad(args[:L-1])
+            self.token = args[0]
+            load = uLoad(args[1:])
             self.__dict__.update(vars(load))
 
 #Reply template for BMS to accept / reject load request
