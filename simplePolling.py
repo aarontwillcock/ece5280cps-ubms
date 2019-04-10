fbe011255b1d24eb3d26ea230f7a203f1fdc2588
Aaron <aarontwillcock@gmail.com>
2019-04-10 08:53:42 -0400

    simplePolling.py - Fixed CLR init

--- simplePolling.py
+++ simplePolling.py
@@ -11,7 +11,7 @@ gpio.setup(13,gpio.IN, pull_up_down=gpio.PUD_DOWN)
 gpio.setup(19,gpio.OUT)
 
 #Disable the clear
-gpio.output(19,gpio.LOW)
+gpio.output(19,gpio.HIGH)
 
 while True:
     if(not gpio.input(13)):
