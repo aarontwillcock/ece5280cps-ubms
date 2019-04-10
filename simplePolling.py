cd51e758ff9aedea07c52246184689c3ab8098a3
Aaron <aarontwillcock@gmail.com>
2019-04-10 08:43:46 -0400

    Extended simple polling

--- simplePolling.py
+++ simplePolling.py
@@ -1,15 +1,28 @@
 #Dependencies
 import RPi.GPIO as gpio
+import time
 
 
 #Initialize gpio to use broadcomm numberings
 gpio.setmode(gpio.BCM)
 
 #Setup pin
-gpio.setup(13,gpio.IN, pull_up_down=gpio.PUD_DOWN)
+gpio.setup(13,gpio.IN, pull_up_down=gpio.PUD_UP)
+gpio.setup(26,gpio.OUT)
+gpio.setup(19,gpio.OUT)
+
+#Turn off shutdown
+gpio.output(26,gpio.LOW)
 
 while True:
     if(gpio.input(13)):
-        print("High")
+        print("Interrupted!")
+    
+        gpio.output(19,gpio.HIGH)
+        gpio.output(19,gpio.LOW)
+    
     else:
-        print("Low")
+        print("Pin high (no interrupt)")
+
+    time.sleep(5)
+    
